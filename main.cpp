769ed77cbde8c5ea78885fe8e4196809be4ec163
CallumDev <callum.mcging@gmail.com>
2022-02-22 20:14:12 +1030

    Disable imgui.ini

--- main.cpp
+++ main.cpp
@@ -40,6 +40,7 @@ int main(int, char**)
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.IniFilename = NULL;
     //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
     //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 
